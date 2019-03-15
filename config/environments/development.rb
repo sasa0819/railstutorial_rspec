12b679a2d1023aaacb32ad78ce0222ece569e49f
sasa <sasauchi.shunsuke@gmail.com>
2019-03-15 13:09:03 +0900

    add divise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "sasauchi.shunsuke@gmail.com",
+    :password => "08191980",
+    :authentication => 'login'
+}
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'toppages#home'
   get   '/help', to: 'toppages#help', as: 'helf'
 end
