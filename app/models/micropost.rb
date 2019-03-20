fc47ad4bf8f66df311c814ac3ab4b2267de13862
sasa <sasauchi.shunsuke@gmail.com>
2019-03-21 00:14:13 +0900

    feat: 画像投稿機能の追加です

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,18 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   default_scope -> { order(created_at: :desc)}
   validates :user_id, presence: true
   validates :content, presence: true
+
+  # def thumbnail
+  #   return self.image.variant(resize: '100*100')
+  # end
+
+  # def image_type
+  # if image.attached? && image.content_type.in?(%("image/jpeg image/png image/jpg"))
+  #   errors.add(:image, 'error message')
+  # elsif !image.attached?
+  #   errors.add(:image, 'error message')
+  # end
 end
