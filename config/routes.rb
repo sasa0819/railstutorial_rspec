159dc800ab7c411d04d61b4ce61d6775e6f88399
sasa <sasauchi.shunsuke@gmail.com>
2019-03-23 23:35:45 +0900

    Add: フォロー、フォロー解除ボタンを実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords',
   }
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts
+  resources :relationships, only: [:create, :destroy]
   get "users/:id" => "users#show", as: :mypage
   root 'toppages#home'
   get '/help', to: 'toppages#help', as: 'helf'
